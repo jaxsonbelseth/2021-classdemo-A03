4fc09c94f9fd3f08eb1f3190bfc22ec05858fb03
jaxsonbelseth <jbelseth1@nait.ca>
2021-01-13 10:15:54 -0700

    Create first sample console program

---
+++ FirstSampleSolution/FirstSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The WriteLine() command will output the
+            //        Hello World to the console window.
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is: \t Jaxson Belseth");
+            Console.WriteLine("I go to: \t NAIT");
+        }
+    }
+}
